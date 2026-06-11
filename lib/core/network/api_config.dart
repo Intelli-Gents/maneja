3a055f2a0bc6bc981142077bee6b5c0b214eff16
Amos-e <amosemmanelz@gmail.com>
2026-06-11 12:57:31 +0300

    Changed baseUrl to point to render test environment

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
   // static const String baseUrl = 'https://maneja-ug.onrender.com/api';
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'https://maneja-ug.onrender.com/api';
 }
